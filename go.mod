6c558da08ec6e9450aebe695672b1aaf2212fe5f
Munkh-Altai Chuluunbaatar <munkh-altai@MacBook-Pro.local>
2022-10-19 15:07:50 +0800

    initional data updated

--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.15
 
 require (
 	github.com/gofiber/fiber/v2 v2.37.0-rc.1
-	github.com/lambda-platform/lambda v0.5.22
+	github.com/lambda-platform/lambda v0.5.35
 	github.com/grokify/html-strip-tags-go v0.0.1
 
 )
--- middleware/bodyDump.go
+++ middleware/bodyDump.go
@@ -1,55 +1,55 @@
 package middleware
 
 import (
-    "encoding/json"
-    "github.com/gofiber/fiber/v2"
-    "github.com/lambda-platform/arcGIS/handlers"
-    "github.com/lambda-platform/lambda/dataform"
-    "github.com/lambda-platform/lambda/datagrid"
-    "github.com/lambda-platform/lambda/utils"
-    "strconv"
+	"encoding/json"
+	"github.com/gofiber/fiber/v2"
+	"github.com/lambda-platform/arcGIS/handlers"
+	"github.com/lambda-platform/lambda/dataform"
+	"github.com/lambda-platform/lambda/datagrid"
+	"github.com/lambda-platform/lambda/utils"
+	"strconv"
 )
 
 type crudResponse struct {
-    Data struct {
-        ID int `gorm:"column:id;" json:"id"`
-    } `json:"data"`
+	Data struct {
+		ID int `gorm:"column:id;" json:"id"`
+	} `json:"data"`
 }
 
 func BodyDump(c *fiber.Ctx, GetGridMODEL func(schema_id string) datagrid.Datagrid, GetMODEL func(schema_id string) dataform.Dataform) error {
-    if err := c.Next(); err != nil {
-        return err
-    }
-    action := c.Params("action")
-    if c.Path() == "/lambda/krud/delete/:schemaId/:id" {
-        action = "delete"
-    }
+	if err := c.Next(); err != nil {
+		return err
+	}
+	action := c.Params("action")
+	if c.Path() == "/lambda/krud/delete/:schemaId/:id" {
+		action = "delete"
+	}
 
-    if action == "store" || action == "update" || action == "delete" {
+	if action == "store" || action == "update" || action == "delete" {
 
-        reqBody := utils.GetBody(c)
-        RowId := ""
-        schemaId, _ := strconv.ParseInt(c.Params("schemaId"), 10, 64)
-        if action == "store" {
+		reqBody := utils.GetBody(c)
+		RowId := ""
+		schemaId, _ := strconv.ParseInt(c.Params("schemaId"), 10, 64)
+		if action == "store" {
 
-            var response crudResponse
+			var response crudResponse
 
-            if err := json.Unmarshal(c.Response().Body(), &response); err != nil {
-                panic(err)
-            }
-            RowId = strconv.Itoa(response.Data.ID)
+			if err := json.Unmarshal(c.Response().Body(), &response); err != nil {
+				panic(err)
+			}
+			RowId = strconv.Itoa(response.Data.ID)
 
-        } else {
-            RowId = c.Params("id")
-        }
+		} else {
+			RowId = c.Params("id")
+		}
 
-        if action == "store" || action == "update" {
-            handlers.SAVEGIS(reqBody, schemaId, action, RowId, GetMODEL)
-        } else if action == "delete" {
-            handlers.DELTEGIS(reqBody, schemaId, action, RowId, GetGridMODEL)
-        }
+		if action == "store" || action == "update" {
+			handlers.SAVEGIS(reqBody, schemaId, action, RowId, GetMODEL)
+		} else if action == "delete" {
+			handlers.DELTEGIS(reqBody, schemaId, action, RowId, GetGridMODEL)
+		}
 
-    }
+	}
 
-    return nil
+	return nil
 }
