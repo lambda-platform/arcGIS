5cabfd19c695e19710b145a1e60df8f0124a99e1
munkh-altai <munkaltai@gmail.com>
2023-12-27 08:50:06 +0800

    origin added

--- arcgis.go
+++ arcgis.go
@@ -37,6 +37,10 @@ func Set(e *fiber.App, GetGridMODEL func(schema_id string) datagrid.Datagrid, Ge
 
 	e.Use("/arcgis", func(c *fiber.Ctx) error {
 		// Create a new request based on the original
+		c.Response().Header.Set("Access-Control-Allow-Origin", "*")
+		c.Request().Header.Set("Access-Control-Allow-Headers", "Origin, Content-Type, Accept, X-Requested-With, x-csrf-token, Accept-Language, Content-Length, Authorization, Accept-Encoding, Connection")
+		c.Request().Header.Set("Access-Control-Allow-Methods", "GET,POST,HEAD,PUT,DELETE,PATCH,OPTIONS")
+
 		req := fasthttp.Request{}
 		url := c.Request().URI()
 		url.SetScheme(target.Scheme)
@@ -45,6 +49,9 @@ func Set(e *fiber.App, GetGridMODEL func(schema_id string) datagrid.Datagrid, Ge
 		req.Header.SetHost(target.Host)
 
 		fasthttpadaptor.NewFastHTTPHandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+			writer.Header().Set("Access-Control-Allow-Origin", "*")
+			writer.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, Accept, X-Requested-With, x-csrf-token, Accept-Language, Content-Length, Authorization, Accept-Encoding, Connection")
+			writer.Header().Set("Access-Control-Allow-Methods", "GET,POST,HEAD,PUT,DELETE,PATCH,OPTIONS")
 
 			proxy.ServeHTTP(writer, request)
 
